d1a9da246604cb4a1e66e00fcd567dd39e81d933
Jhonny Mosquera <jhonnymosquera.web@gmail.com>
2024-02-12 17:55:12 -0500

    pruebas cpp

--- C++/99_mis_pruebas/otros/main.cpp
+++ C++/99_mis_pruebas/otros/main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
+
+int casa[5][5];
+
+void setColor(int valor)
+{
+    if (valor != 0)
+    {
+        cout << "\e[0;35m";
+    }
+    else
+    {
+        cout << "\033[0m";
+    }
+}
 
 int main()
 {
+    int n = 5; // tamaño
 
-    int numero1 = 15;
-    int numero2 = 2;
-    int residuo = numero1 % numero2;
+    // llenamos la casa
+    for (int p = 0; p < n; p++)
+    {
+        for (int h = 0; h < n; h++)
+        {
+            if (h == 0 || p == 0 && h == 1 || p == 0 && h == 2 || p == 0 && h == 3 || p == 1 && h == 3 || p == 2 && h == 1 || p == 2 && h == 2 || p == 3 && h == 3 || p == 4 && h == 1 || p == 4 && h == 2 || p == 4 && h == 3)
+            {
 
-    cout << "Residuo es: " << residuo << endl;
+                casa[p][h] = 1;
+            }
+        }
+    }
+
+    for (int p = 0; p < n; p++)
+    {
+        for (int h = 0; h < n; h++)
+        {
+            setColor(casa[p][h]);
+            cout << "" << casa[p][h];
+        }
+        cout << "\033[0m\n";
+    }
 
     return 0;
 }
